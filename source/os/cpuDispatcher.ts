277bc8221bef67584d0ed655f6c4ffbc2307bcad
BrendonKupsch1 <98063893+BrendonKupsch1@users.noreply.github.com>
2023-11-04 19:53:49 +0000

    Started CPU Dispatcher

---
+++ source/os/cpuDispatcher.ts
@@ -0,0 +1,17 @@
+/* ------------
+cpuDispatcher.ts
+
+CPU Dispatcher
+The CPU Dispatcher handles context switches from the CPU Scheduler.
+------------  */
+
+module TSOS {
+
+    export class CpuDispatcher {
+
+        constructor() {
+        }
+
+        public contextSwitch() {}
+    }
+}
