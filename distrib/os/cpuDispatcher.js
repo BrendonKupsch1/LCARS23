11ffb4ea68a25c5d4c18b184aed65eaaa3b0b39b
BrendonKupsch1 <98063893+BrendonKupsch1@users.noreply.github.com>
2023-11-04 20:23:27 +0000

    CPU Dispatcher written, need to do a lot of debuging.

--- distrib/os/cpuDispatcher.js
+++ distrib/os/cpuDispatcher.js
@@ -4,15 +4,12 @@ cpuDispatcher.ts
 CPU Dispatcher
 The CPU Dispatcher handles context switches from the CPU Scheduler.
 ------------  */
-
-module TSOS {
-
-    export class CpuDispatcher {
-
+var TSOS;
+(function (TSOS) {
+    class CpuDispatcher {
         constructor() {
         }
-
-        public contextSwitch() {
+        contextSwitch() {
             if (_MemoryManager.readyQueue.getSize() > 0 && _CPU.currentPCB === null) {
                 var nextProcess = _MemoryManager.readyQueue.dequeue();
                 _CpuScheduler.executingPCB = nextProcess;
@@ -25,12 +22,14 @@ module TSOS {
                 TSOS.Control.updatePcbDisplay(false, _CpuScheduler.executingPCB);
                 // get next process and update current process
                 var NextProcess = _MemoryManager.readyQueue.dequeue();
-            _CpuScheduler.executingPCB = nextProcess;
-            _CPU.loadNewProcess(_CpuScheduler.executingPCB);
+                _CpuScheduler.executingPCB = nextProcess;
+                _CPU.loadNewProcess(_CpuScheduler.executingPCB);
             }
             else {
-                alert("Ready queue is empty.")
+                alert("Ready queue is empty.");
             }
         }
     }
-}
+    TSOS.CpuDispatcher = CpuDispatcher;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=cpuDispatcher.js.map
