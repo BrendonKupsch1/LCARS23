c4b2a5f3888a9566378a199ae6e029de045477f3
BuildTools <unconfigured@null.spigotmc.org>
2023-12-12 16:20:13 -0500

    Created CPU Swapper module

---
+++ source/os/cpuSwapper.ts
@@ -0,0 +1,13 @@
+/* ---------------
+    cpuSwapper.ts
+
+    CPU Swapper
+    The swapper works with the CPU schelduer and CPU dispatcher to swap processes in and out of memory
+    ------------ */
+
+module TSOS {
+
+    export class CpuSwapper {
+
+    }
+}
